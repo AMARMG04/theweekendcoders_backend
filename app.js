5562592b214a92c4976789665a75295229a56634
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2023-12-05 13:42:50 +0530

    Changes in the backend

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 const corsOptions = {
   // origin: process.env.NODE_ENV === "production" ? "https://theweekendcoders.vercel.app/" : "*",
   origin: "https://theweekendcoders.vercel.app/",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  methods: ["GET", "PUT", "POST"],
   credentials: true, // enable set cookie
   optionsSuccessStatus: 204,
 };
