3ad4ad528b13c42a535f2ed0dee26667d05a9cfe
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2023-12-07 17:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const emailTemplate = `
   `;
 // Define route for form submission
 app.post("/submit-form", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://theweekendcoders.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
